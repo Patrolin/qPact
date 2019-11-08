830a841d496d68b7268d1f0f818bc46b91e23fcd
Patrolin <patrik@balas.cz>
2019-11-08 20:42:01 +0100

    idek

--- src/qPact.js/Pact/components/katex.js
+++ src/qPact.js/Pact/components/katex.js
@@ -1,22 +1,22 @@
 class Katex extends module.Component {
 	async first() {
-		if (global['katex'] === undefined) {
+		if (global.katex === undefined) {
 			return new Promise(function(resolve, reject) {
 				let head = module.q(/head/);
 				head.q(
 					`<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css" integrity="sha384-zB1R0rpPzHqg7Kpt0Aljp8JPLqbXI3bhnPWROx27a9N0Ll6ZP/+DiW/UqRcLbRjq" crossorigin="anonymous">`
 				);
-				let katex = document.createElement('script');
-				Object.assign(katex, {
+				let script = document.createElement('script');
+				Object.assign(script, {
 					src:
 						'https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.js',
 					integrity:
 						'sha384-y23I5Q6l+B6vatafAwxRu/0oK/79VlbSz7Q9aiSZUvyWYIYsd+qj+o24G5ZU2zJz',
 					crossOrigin: 'anonymous',
 				});
-				katex.onload = resolve;
-				katex.onerror = reject;
-				head.q(katex);
+				script.onload = resolve;
+				script.onerror = reject;
+				head.q(script);
 			});
 		}
 	}
