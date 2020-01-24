bc041438f9080511da46d5fdbdd453c9e8a33393
Patrolin <patrik@balas.cz>
2020-01-24 15:18:32 +0100

    some css fixes

---
+++ dist/lib.js
@@ -0,0 +1,254 @@
+let lib = new function() {
+	'use strict';
+	let t = this, e = window, r = void 0, n = null, i = !0;
+	t.isPrimitive = function(t) {
+		return t !== Object(t);
+	};
+	t.isNative = function(t) {
+		return /\[native code\]/.test(t.name);
+	};
+	t.isBoolean = function(t) {
+		return 'boolean' == typeof t;
+	};
+	t.isString = function(t) {
+		return 'string' == typeof t;
+	};
+	t.isNumber = function(t) {
+		return 'number' == typeof t;
+	};
+	t.MAX_INT32 = -(t.MIN_INT32 = -2147483648) - 1;
+	t.MAX_UINT32 = 4294967295;
+	t.MAX_INT64 = -(t.MIN_INT64 = -Math.pow(2, 63)) - 1;
+	t.MAX_UINT64 = Math.pow(2, 64) - 1;
+	t.MAX_INT = -(t.MIN_INT = Number.MIN_SAFE_INTEGER - 1);
+	t.WEEK = 7 * (t.DAY = 24 * (t.HOUR = 60 * (t.MINUTE = 60 * (t.SECOND = 1e3))));
+	t.MONTH = (t.YEAR = 365.2425 * t.DAY) / 12;
+	t.DateTime = class DateTime extends Date {
+		get y() {
+			this.getFullYear();
+		}
+		set y(t) {
+			this.setFullYear(t);
+		}
+		get o() {
+			this.getMonth();
+		}
+		set o(t) {
+			this.setMonth(t);
+		}
+		get d() {
+			this.getDate();
+		}
+		set d(t) {
+			this.setDate(t);
+		}
+		get h() {
+			this.getHours();
+		}
+		set h(t) {
+			this.setHours(t);
+		}
+		get m() {
+			this.getMinutes();
+		}
+		set m(t) {
+			this.setMinutes(t);
+		}
+		get s() {
+			this.getSeconds();
+		}
+		set s(t) {
+			this.setSeconds(t);
+		}
+		get ms() {
+			this.getMilliseconds();
+		}
+		set ms(t) {
+			this.setMilliseconds(t);
+		}
+		constructor(t = "") {
+			super();
+			this.modify(t);
+		}
+		modify(t = "") {
+			o(this, t);
+		}
+		format(t) {
+			return new Intl.DateTimeFormat(r, {
+				weekday: 'long',
+				day: 'numeric',
+				month: 'long',
+				year: 'numeric',
+				hour: 'numeric',
+				minute: 'numeric',
+				second: 'numeric',
+				timeZoneName: 'long',
+				...t
+			}).format(this);
+		}
+	};
+	t.TimeInterval = class {
+		constructor(t) {
+			let e = this;
+			e.y = e.o = e.d = e.h = e.m = e.s = e.ms = 0;
+			t && e.modify(t);
+		}
+		valueOf() {
+			let {y: e, o: r, d: n, h: i, m: s, s: o, ms: a} = this;
+			return e * t.YEAR + r * t.MONTH + n * t.DAY + i * t.HOUR + s * t.MINUTE + o * t.SECOND + a;
+		}
+		modify(e = "") {
+			let r = this;
+			o(r, e);
+			let n = +r;
+			n *= r.sign = Math.sign(n);
+			n -= r.ms = n % t.SECOND;
+			n -= r.s = n % t.MINUTE;
+			n -= r.m = n % t.HOUR;
+			n -= r.h = n % t.DAY;
+			n -= r.d = n % t.MONTH;
+			n -= r.o = n % t.YEAR;
+			r.y = n;
+			r.s /= t.SECOND;
+			r.m /= t.MINUTE;
+			r.h /= t.HOUR;
+			r.d /= t.DAY;
+			r.o /= t.MONTH;
+			r.y /= t.YEAR;
+		}
+		toString() {
+			let {y: e, o: r, d: n, h: i, m: s, s: o, ms: a, sign: u} = this, l = {};
+			e && (l.years = e);
+			r && (l.months = r);
+			n && (l.days = n);
+			i && (l.hours = i);
+			s && (l.minutes = s);
+			o && (l.seconds = o);
+			a && (l.milliseconds = a);
+			return `${[ '', '-', '+' ][u + 1]}${t.items(l).map(([t, e]) => `${e}${e > 1 ? t : t.slice(0, -1)}`).join(' ')}`;
+		}
+	};
+	let s = [ 'sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday' ];
+	function o(t, e) {
+		let r = 1;
+		for (let [n, i, o, a, u, l] of e.matchAll(/(\d+)([a-z]+)|([a-z]+)(\d+)|([+-])/g)) if (i) {
+			let e = s.indexOf(o);
+			if (0 > e && o in t) t[o] += r * i; else {
+				let n = t.getDate(), s = t.getDay();
+				if (e != s) {
+					n += r > 0 ? e - s + 7 : e + s - 15;
+					i--;
+				}
+				t.setDate(n + 7 * r * i);
+			}
+		} else u && a in t ? t[a] = +u : l && (r = '+' == l ? 1 : -1);
+	}
+	t.assoc_keys = function(t) {
+		let e = t[Symbol.iterator];
+		if (e) {
+			let r = 'entries' === e.name, n = Array.from(e.call(t));
+			return [ r, $1r ? n.map(([t]) => t) : n.map((t, e) => e) ];
+		}
+		{
+			let e = [];
+			for (let r in t) e.push(r);
+			return [ i, e ];
+		}
+	};
+	t.assoc_values = function(t) {
+		let e = t[Symbol.iterator];
+		if (e) {
+			let r = 'entries' === e.name, n = Array.from(e.call(t));
+			r && (n = n.map(([t, e]) => e));
+			return [ r, n ];
+		}
+		{
+			let e = [];
+			for (let r in t) e.push(t[r]);
+			return [ i, e ];
+		}
+	};
+	t.assoc_items = function(t) {
+		let e = t[Symbol.iterator];
+		if (e) {
+			let r = 'entries' === e.name, n = Array.from(e.call(t));
+			r || (n = n.map((t, e) => [ e, t ]));
+			return [ r, n ];
+		}
+		{
+			let e = [];
+			for (let r in t) e.push([ r, t[r] ]);
+			return [ i, e ];
+		}
+	};
+	t.keys = function(e) {
+		return t.assoc_keys(e)[1];
+	};
+	t.values = function(e) {
+		return t.assoc_values(e)[1];
+	};
+	t.items = function(e) {
+		return t.assoc_items(e)[1];
+	};
+	t.bool = function(e = i) {
+		return e && (t.isPrimitive(e) || e.length);
+	};
+	t.int = function(e = 0) {
+		let r = Math.floor(+e);
+		if (Object.is(r, NaN)) throw TypeError(`Cannot convert ${describe(e)} to int`);
+		if (r < t.MIN_INT || r > t.MAX_INT) throw RangeError(`${describe(e)} must be in range <${t.MIN_INT}, ${t.MAX_INT}>`);
+		return r;
+	};
+	t.float = function(t = 0) {
+		if (isNaN(+t)) throw TypeError(`Cannot convert ${describe(t)} to float`);
+		return +t;
+	};
+	t.str = function(e = "") {
+		if (t.isPrimitive(e)) return e + "";
+		{
+			let [r, n] = assoc_items(e);
+			return r ? `{${n.map(([e, r]) => `${t.isString(e) ? `"${e}"` : `${e}`}: ${str(r)}`).join(', ')}}` : `[${n.map(([t, e]) => str(e)).join(', ')}]`;
+		}
+	};
+	t.list = function(e = []) {
+		if (t.isPrimitive(e) && !t.isString(e)) throw TypeError(`Cannot convert ${describe(e)} to list`);
+		return Array.from(e);
+	};
+	t.set = function(t = new Set()) {
+		if (t[Symbol.iterator]) return new Set(t);
+		throw TypeError(`Cannot convert ${describe(t)} to set`);
+	};
+	t.dict = function(t = new Map()) {
+		try {
+			return new Map(t instanceof Object ? Object.entries(t) : 'string' == typeof t ? value = JSON.parse(t) : t);
+		} catch (e) {
+			throw TypeError(`Cannot convert ${describe(t)} to map`);
+		}
+	};
+	t.obj = function(t = {}) {
+		if (t && t.letructor === Object) return t;
+		{
+			let e = 'string' == typeof t ? JSON.parse(t) : t;
+			if (e[Symbol.iterator]) {
+				let t = {};
+				for (let [r, n] of e) t[r] = n;
+				return t;
+			}
+			if (e instanceof Object) return e;
+			throw TypeError(`Cannot convert ${describe(t)} to dict`);
+		}
+	};
+	t.describe = function(t) {
+		return `${t != n ? `${t.constructor.name}: ` : ''}${t}`;
+	};
+	t.matchAll = function(t, e) {
+		let r, i = [];
+		for (;(r = e.exec(t)) !== n; ) i.push(r);
+		return i;
+	};
+	t.divmod = function(t, e) {
+		let r = t / e;
+		return [ 0 > r ? Math.ceil(r) : Math.floor(r), t % e ];
+	};
+	for (let r in t) r in e ? t.isNative(e[r]) || console.warn(`lib: ${r} is already defined`) : e[r] = t[r];
+}();
---
+++ dist/qPact.js
@@ -0,0 +1,430 @@
+let qPact = new function() {
+	'use strict';
+	let e = this, t = window, n = void 0, i = null, o = !0;
+	e.q = function(e, t = n) {
+		return e instanceof RegExp ? a(document, e, t) : r(e, t);
+	};
+	function r(t, n) {
+		if (t instanceof Node) return l(t, n);
+		if (t instanceof Array) return l(t.map(t => e.q(t)), n);
+		let i = document.createElement('template');
+		i.innerHTML = t;
+		let o = i.content.childNodes;
+		return l(1 === o.length ? o[0] : [ ...o ], n);
+	}
+	function l(e, t) {
+		return t === n ? e : Array(t).fill().map(() => s(e));
+	}
+	function s(e) {
+		return e instanceof Node ? e.cloneNode(o) : e.map(s);
+	}
+	Node.prototype.Q = function(e, t = n) {
+		let o, r = this, l = {};
+		for (;(o = r.firstChild) !== i; ) {
+			r.removeChild(o);
+			o.id && (l[o.id] = o);
+		}
+		if (e === i) return [];
+		let s = r.q(e, t);
+		for (let e of r.querySelectorAll('[id]')) e.id in l && !e.childNodes.length && e.parentNode.replaceChild(l[e.id], e);
+		return s instanceof Node ? s : s.map(e => e.id ? l[e.id] : e);
+	};
+	Node.prototype.q = function(e, t = n) {
+		if (e instanceof RegExp) return a(this, e, t);
+		let i = r(e, t);
+		return i instanceof Node ? this.appendChild(i) : i.map(e => this.appendChild(e));
+	};
+	function a(e, t, n) {
+		if (t.global) return l(e.querySelectorAll(t.source), n);
+		let o = e.querySelector(t.source);
+		return o ? l(o, n) : i;
+	}
+	Array.prototype.Q = function(e, t = n) {
+		return this.map(n => n.Q(e, t));
+	};
+	Array.prototype.q = function(e, t = n) {
+		return e instanceof RegExp ? l(e.global ? c(this, e.source) : f(this, e.source), t) : this.map(n => n.q(e, t));
+	};
+	function c(e, t) {
+		return e.map(e => e instanceof Node ? e.matches(t) ? e : i : c(e, t)).filter(e => e !== i);
+	}
+	function f(e, t) {
+		return e.flat(1 / 0).find(e => e.matches(t));
+	}
+	e.isPrimitive = function(e) {
+		return e !== Object(e);
+	};
+	e.isNative = function(e) {
+		return /\[native code\]/.test(e.name);
+	};
+	e.isBoolean = function(e) {
+		return 'boolean' == typeof e;
+	};
+	e.isString = function(e) {
+		return 'string' == typeof e;
+	};
+	e.isNumber = function(e) {
+		return 'number' == typeof e;
+	};
+	e.MAX_INT32 = -(e.MIN_INT32 = -2147483648) - 1;
+	e.MAX_UINT32 = 4294967295;
+	e.MAX_INT64 = -(e.MIN_INT64 = -Math.pow(2, 63)) - 1;
+	e.MAX_UINT64 = Math.pow(2, 64) - 1;
+	e.MAX_INT = -(e.MIN_INT = Number.MIN_SAFE_INTEGER - 1);
+	e.WEEK = 7 * (e.DAY = 24 * (e.HOUR = 60 * (e.MINUTE = 60 * (e.SECOND = 1e3))));
+	e.MONTH = (e.YEAR = 365.2425 * e.DAY) / 12;
+	e.DateTime = class DateTime extends Date {
+		get y() {
+			this.getFullYear();
+		}
+		set y(e) {
+			this.setFullYear(e);
+		}
+		get o() {
+			this.getMonth();
+		}
+		set o(e) {
+			this.setMonth(e);
+		}
+		get d() {
+			this.getDate();
+		}
+		set d(e) {
+			this.setDate(e);
+		}
+		get h() {
+			this.getHours();
+		}
+		set h(e) {
+			this.setHours(e);
+		}
+		get m() {
+			this.getMinutes();
+		}
+		set m(e) {
+			this.setMinutes(e);
+		}
+		get s() {
+			this.getSeconds();
+		}
+		set s(e) {
+			this.setSeconds(e);
+		}
+		get ms() {
+			this.getMilliseconds();
+		}
+		set ms(e) {
+			this.setMilliseconds(e);
+		}
+		constructor(e = "") {
+			super();
+			this.modify(e);
+		}
+		modify(e = "") {
+			u(this, e);
+		}
+		format(e) {
+			return new Intl.DateTimeFormat(n, {
+				weekday: 'long',
+				day: 'numeric',
+				month: 'long',
+				year: 'numeric',
+				hour: 'numeric',
+				minute: 'numeric',
+				second: 'numeric',
+				timeZoneName: 'long',
+				...e
+			}).format(this);
+		}
+	};
+	e.TimeInterval = class {
+		constructor(e) {
+			let t = this;
+			t.y = t.o = t.d = t.h = t.m = t.s = t.ms = 0;
+			e && t.modify(e);
+		}
+		valueOf() {
+			let {y: t, o: n, d: i, h: o, m: r, s: l, ms: s} = this;
+			return t * e.YEAR + n * e.MONTH + i * e.DAY + o * e.HOUR + r * e.MINUTE + l * e.SECOND + s;
+		}
+		modify(t = "") {
+			let n = this;
+			u(n, t);
+			let i = +n;
+			i *= n.sign = Math.sign(i);
+			i -= n.ms = i % e.SECOND;
+			i -= n.s = i % e.MINUTE;
+			i -= n.m = i % e.HOUR;
+			i -= n.h = i % e.DAY;
+			i -= n.d = i % e.MONTH;
+			i -= n.o = i % e.YEAR;
+			n.y = i;
+			n.s /= e.SECOND;
+			n.m /= e.MINUTE;
+			n.h /= e.HOUR;
+			n.d /= e.DAY;
+			n.o /= e.MONTH;
+			n.y /= e.YEAR;
+		}
+		toString() {
+			let {y: t, o: n, d: i, h: o, m: r, s: l, ms: s, sign: a} = this, c = {};
+			t && (c.years = t);
+			n && (c.months = n);
+			i && (c.days = i);
+			o && (c.hours = o);
+			r && (c.minutes = r);
+			l && (c.seconds = l);
+			s && (c.milliseconds = s);
+			return `${[ '', '-', '+' ][a + 1]}${e.items(c).map(([e, t]) => `${t}${t > 1 ? e : e.slice(0, -1)}`).join(' ')}`;
+		}
+	};
+	let d = [ 'sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday' ];
+	function u(e, t) {
+		let n = 1;
+		for (let [i, o, r, l, s, a] of t.matchAll(/(\d+)([a-z]+)|([a-z]+)(\d+)|([+-])/g)) if (o) {
+			let t = d.indexOf(r);
+			if (0 > t && r in e) e[r] += n * o; else {
+				let i = e.getDate(), r = e.getDay();
+				if (t != r) {
+					i += n > 0 ? t - r + 7 : t + r - 15;
+					o--;
+				}
+				e.setDate(i + 7 * n * o);
+			}
+		} else s && l in e ? e[l] = +s : a && (n = '+' == a ? 1 : -1);
+	}
+	e.assoc_keys = function(e) {
+		let t = e[Symbol.iterator];
+		if (t) {
+			let n = 'entries' === t.name, i = Array.from(t.call(e));
+			return [ n, $1n ? i.map(([e]) => e) : i.map((e, t) => t) ];
+		}
+		{
+			let t = [];
+			for (let n in e) t.push(n);
+			return [ o, t ];
+		}
+	};
+	e.assoc_values = function(e) {
+		let t = e[Symbol.iterator];
+		if (t) {
+			let n = 'entries' === t.name, i = Array.from(t.call(e));
+			n && (i = i.map(([e, t]) => t));
+			return [ n, i ];
+		}
+		{
+			let t = [];
+			for (let n in e) t.push(e[n]);
+			return [ o, t ];
+		}
+	};
+	e.assoc_items = function(e) {
+		let t = e[Symbol.iterator];
+		if (t) {
+			let n = 'entries' === t.name, i = Array.from(t.call(e));
+			n || (i = i.map((e, t) => [ t, e ]));
+			return [ n, i ];
+		}
+		{
+			let t = [];
+			for (let n in e) t.push([ n, e[n] ]);
+			return [ o, t ];
+		}
+	};
+	e.keys = function(t) {
+		return e.assoc_keys(t)[1];
+	};
+	e.values = function(t) {
+		return e.assoc_values(t)[1];
+	};
+	e.items = function(t) {
+		return e.assoc_items(t)[1];
+	};
+	e.bool = function(t = o) {
+		return t && (e.isPrimitive(t) || t.length);
+	};
+	e.int = function(t = 0) {
+		let n = Math.floor(+t);
+		if (Object.is(n, NaN)) throw TypeError(`Cannot convert ${describe(t)} to int`);
+		if (n < e.MIN_INT || n > e.MAX_INT) throw RangeError(`${describe(t)} must be in range <${e.MIN_INT}, ${e.MAX_INT}>`);
+		return n;
+	};
+	e.float = function(e = 0) {
+		if (isNaN(+e)) throw TypeError(`Cannot convert ${describe(e)} to float`);
+		return +e;
+	};
+	e.str = function(t = "") {
+		if (e.isPrimitive(t)) return t + "";
+		{
+			let [n, i] = assoc_items(t);
+			return n ? `{${i.map(([t, n]) => `${e.isString(t) ? `"${t}"` : `${t}`}: ${str(n)}`).join(', ')}}` : `[${i.map(([e, t]) => str(t)).join(', ')}]`;
+		}
+	};
+	e.list = function(t = []) {
+		if (e.isPrimitive(t) && !e.isString(t)) throw TypeError(`Cannot convert ${describe(t)} to list`);
+		return Array.from(t);
+	};
+	e.set = function(e = new Set()) {
+		if (e[Symbol.iterator]) return new Set(e);
+		throw TypeError(`Cannot convert ${describe(e)} to set`);
+	};
+	e.dict = function(e = new Map()) {
+		try {
+			return new Map(e instanceof Object ? Object.entries(e) : 'string' == typeof e ? value = JSON.parse(e) : e);
+		} catch (t) {
+			throw TypeError(`Cannot convert ${describe(e)} to map`);
+		}
+	};
+	e.obj = function(e = {}) {
+		if (e && e.letructor === Object) return e;
+		{
+			let t = 'string' == typeof e ? JSON.parse(e) : e;
+			if (t[Symbol.iterator]) {
+				let e = {};
+				for (let [n, i] of t) e[n] = i;
+				return e;
+			}
+			if (t instanceof Object) return t;
+			throw TypeError(`Cannot convert ${describe(e)} to dict`);
+		}
+	};
+	e.describe = function(e) {
+		return `${e != i ? `${e.constructor.name}: ` : ''}${e}`;
+	};
+	e.matchAll = function(e, t) {
+		let n, o = [];
+		for (;(n = t.exec(e)) !== i; ) o.push(n);
+		return o;
+	};
+	e.divmod = function(e, t) {
+		let n = e / t;
+		return [ 0 > n ? Math.ceil(n) : Math.floor(n), e % t ];
+	};
+	e.styles = (e.q(/#qPact/) || e.q(/head/).q('<div id="qPact">')).q("<style>body,html{width:100%;height:100%}*{box-sizing:border-box;display:inline-block;position:relative;padding:0;flex-basis:auto;flex-grow:0;flex-shrink:0;pointer-events:auto}body{display:block;margin:0}area,datalist,head,link,param,script,style,title{display:none}li{display:list-item}[table],table{display:inline-table;border-collapse:collapse;border-spacing:0}tbody{display:table-row-group}[caption],caption{display:table-caption}col{display:table-column}colgroup{display:table-column-group}tfoot{display:table-footer-group}thead{display:table-header-group}[table] > *,tbody > *{display:table-row}[table] > * > *,tbody > * > *{display:table-cell}align{display:inline-flex;flex-direction:column;justify-content:center;align-items:center;text-align:center}align[up]{justify-content:start}align[left]{align-items:start;text-align:left}align[down]{justify-content:end}align[right]{align-items:end;text-align:right}blank{visibility:hidden}[hidden]{display:none}flex{display:inline-flex;flex-direction:row}flex > *{flex-grow:60;flex-shrink:60}flex[equal] > *{flex-basis:0}flex[up]{flex-direction:column-reverse}flex[down]{flex-direction:column}flex[left]{flex-direction:row-reverse}[no-flex],[no-grow]{flex-grow:0}[no-flex],[no-shrink]{flex-shrink:0}[f-5]{flex:5}[f-6]{flex:6}[f-10]{flex:10}[f-12]{flex:12}[f-15]{flex:15}[f-20]{flex:20}[f-30]{flex:30}[f-120]{flex:120}[f-180]{flex:180}[f-240]{flex:240}[f-300]{flex:300}[f-360]{flex:360}[f-600]{flex:600}[f-720]{flex:720}[full],[wide],full,wide{width:100%}[full],[tall],full,tall{height:100%}stack{position:relative}stack > *{position:absolute;left:0;top:0;pointer-events:none}tooltip{display:none;position:absolute;left:50%;top:50%;font-weight:bold;font-size:0.35em;padding:0.2em 0.3em;border-radius:0.5em;transform:translate(-50%,-50%)}*:hover > tooltip{display:inline-block}tooltip[right]{left:100%}tooltip[up]{top:0}tooltip[left]{left:0}tooltip[down]{top:100%");
+	e.Component = class Component extends HTMLElement {
+		static set style(t) {
+			let n = this.elementName;
+			(e.styles.q(RegExp(`#${n}`)) || e.styles.q(`<style id="${n}">`)).innerHTML = `${n}{${e.items(t).map(([e, t]) => `${e}:${t}`).join(';')}`;
+		}
+		constructor() {
+			super();
+			let e = this, t = e.constructor;
+			e.state = {
+				...t.state
+			};
+			for (let n of t.events) e.addEventListener(n.slice(2), t => e[n].call(e, t));
+		}
+		async connectedCallback() {
+			let t = this;
+			try {
+				let n = t.constructor;
+				for (let [i, o] of e.items(n.state)) if (t.hasAttribute(i)) try {
+					t[i] = t.getAttribute(i) || o;
+				} finally {}
+				await t.load();
+			} catch (e) {
+				console.error(t, e);
+			}
+		}
+		async disconnectedCallback() {
+			let e = this;
+			try {
+				await e.unload();
+			} catch (t) {
+				console.error(e, t);
+			}
+		}
+		load() {}
+		unload() {}
+		alter() {
+			this.dispatchEvent(new CustomEvent('alter', {
+				bubbles: o,
+				cancelable: o
+			}));
+		}
+	};
+	e.defineElement = function(t, i) {
+		if (!/-/.test(t)) throw SyntaxError("Custom element name must contain '-'");
+		let o = {
+			...i.state
+		}, r = new Set(), l = i.prototype;
+		for (let t of Reflect.ownKeys(l)) {
+			let i = Object.getOwnPropertyDescriptor(l, t);
+			if (i.set) {
+				i.get || Object.defineProperty(l, t, {
+					...i,
+					get: function() {
+						return this.state[t];
+					}
+				});
+				o[t] === n && (o[t] = n);
+			} else e.isString(t) && t.startsWith('on') && r.add(t);
+		}
+		i.state = o;
+		i.events = r;
+		i.elementName = t;
+		return customElements.define(t, i);
+	};
+	class Blink extends e.Component {}
+	Blink.style = {};
+	e.defineElement('q-blink', Blink);
+	class Clock extends e.Component {
+		load() {
+			let e = this;
+			e.interval = setInterval(() => {
+				e.render();
+			}, 1e3);
+			e.render();
+		}
+		unload() {
+			clearInterval(this.interval);
+		}
+		render() {
+			this.Q(new Date());
+		}
+	}
+	e.defineElement('q-clock', Clock);
+	class Copy extends e.Component {
+		onclick() {
+			let e = this;
+			e.focus();
+			let t = e.contentEditable;
+			e.contentEditable = o;
+			document.execCommand('selectAll');
+			e.contentEditable = t;
+			try {
+				document.execCommand('copy');
+			} catch (t) {
+				navigator.clipboard.writeText(e.innerText);
+			}
+		}
+	}
+	e.defineElement('q-copy', Copy);
+	class Katex extends e.Component {
+		init() {
+			if (t.katex === n) return new Promise(function(t, n) {
+				let i = e.q(/head/);
+				i.q("<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css\" integrity=\"sha384-zB1R0rpPzHqg7Kpt0Aljp8JPLqbXI3bhnPWROx27a9N0Ll6ZP/+DiW/UqRcLbRjq\" crossorigin=\"anonymous\">");
+				let o = document.createElement('script');
+				Object.assign(o, {
+					src: 'https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.js',
+					integrity: 'sha384-y23I5Q6l+B6vatafAwxRu/0oK/79VlbSz7Q9aiSZUvyWYIYsd+qj+o24G5ZU2zJz',
+					crossOrigin: 'anonymous'
+				});
+				o.onload = t;
+				o.onerror = n;
+				i.q(o);
+			});
+		}
+		set value(t) {
+			let n = this;
+			t = e.str(t);
+			n.setAttribute('value', t);
+			katex.render(n.state.value = t, n, {
+				displayMode: o
+			});
+		}
+	}
+	e.defineElement('q-katex', Katex);
+	for (let n in e) n in t ? e.isNative(t[n]) || console.warn(`qPact: ${n} is already defined`) : t[n] = e[n];
+}();
