52a131da9618b30c7f5396c6468ded3c7d49762f
Patrolin <patrik@balas.cz>
2020-01-31 22:55:38 +0100

    ctf yay

--- docs/ctf/danger_zone/create_flag.php
+++ docs/ctf/danger_zone/create_flag.php
@@ -1,2 +1,11 @@
 <?php
-var_dump($_POST);
+
+header('Content-Type: application/json');
+
+$flag = $_POST['flag'];
+$time_start = $_POST['time_start'];
+$time_end = $_POST['time_end'];
+
+error_log("$flag, $time_start, $time_end");
+
+echo "{\"success\": true, \"message\": \"you are awesome\"}";
